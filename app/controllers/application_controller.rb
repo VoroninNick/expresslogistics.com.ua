73ed9fbe2db8e9d68109df79f7bd064b9d0f9327
Vik Ewoods <support@voroninstudio.eu>
2013-02-16 11:59:29 +0200

    Move action to header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,33 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  if request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first == 'en'
 
-    cookies[:language] = {
-        :value => 'eng',
-        :expires => 1.years.from_now.utc
-    }
-
-  elsif request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first == 'ru'
-
-    cookies[:language] = {
-        :value => 'rus',
-        :expires => 1.years.from_now.utc
-    }
-
-  elsif request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first == 'ua'
-
-    cookies[:language] = {
-        :value => 'ukr',
-        :expires => 1.years.from_now.utc
-    }
-
-  elsif request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first == 'de'
-
-    cookies[:language] = {
-        :value => 'de',
-        :expires => 1.years.from_now.utc
-    }
-    
-  end
 end
